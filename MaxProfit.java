0d9c5bcaed93d9856b5cf79e557cbdc5174841a9
Abhishek Kumar <ytabhishek2002@gmail.com>
2025-08-01 20:17:41 +0530

    Update MaxProfit.java
    Updated the  variables declaration and changed the approach to geeting max profit

--- MaxProfit.java
+++ MaxProfit.java
@@ -1,10 +1,10 @@
 public class MaxProfit {
     public static void main(String[] args) {
-        int[] prices1 = {7, 1, 5, 3, 6, 4};
-        int[] prices2 = {7, 6, 4, 3, 1};
+        int[] First_Price = {7, 1, 5, 3, 6, 4};
+        int[] Second_Price = {7, 6, 4, 3, 1};
 
-        System.out.println("Max Profit for prices1: " + maxProfit(prices1));
-        System.out.println("Max Profit for prices2: " + maxProfit(prices2));
+        System.out.println("Max Profit for prices1: " + maxProfit(First_Price));
+        System.out.println("Max Profit for prices2: " + maxProfit(Second_Price));
     }
 
     public static int maxProfit(int[] prices) {
